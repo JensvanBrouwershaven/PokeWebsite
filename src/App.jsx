cf229186653e2ce08f838fb6135ffee47cfe3a4e
nikayvdl <9015853@student.zadkine.nl>
2025-07-02 10:54:37 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
+// App.js
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cards from './pages/ProductPage';
 import Profile from './pages/Profiel';
-import Nav from './components/Nav'
+import Nav from './components/Nav';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Cards />} />
-        <Route path='/profiel' element={<Profile />} />
-        <Route path='/Nav' element={<Nav />} />
-      </Routes>
+      {/* Nav wrapt hier de Routes */}
+      <Nav>
+        <Routes>
+          <Route path='/' element={<Cards />} />
+          <Route path='/profiel' element={<Profile />} />
+        </Routes>
+      </Nav>
     </BrowserRouter>
   );
 }
